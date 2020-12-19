fc8e53cc840ee34440c4bec54ca2124ca2a91119
Ryan <rypoon18@gmail.com>
2020-12-19 13:37:51 -0500

    lambda

--- src/functions/update.js
+++ src/functions/update.js
@@ -1,31 +1,31 @@
-const path = require('path');
-const createCsvWriter = require('csv-writer').createArrayCsvWriter;
-const d3 = require('d3-fetch');
-if (typeof fetch !== 'function') {
-    global.fetch = require('node-fetch');
-}
+// const path = require('path');
+// const createCsvWriter = require('csv-writer').createArrayCsvWriter;
+// const d3 = require('d3-fetch');
+// if (typeof fetch !== 'function') {
+//     global.fetch = require('node-fetch');
+// }
 
-let records = [];
+// let records = [];
 
-const dayDifference = (current, target) => (current.getTime() - target.getTime()) / (1000 * 60 * 60 * 24);
-let currentDate = new Date(new Date().getTime() - (36 * 60 * 60 * 1000));
+// const dayDifference = (current, target) => (current.getTime() - target.getTime()) / (1000 * 60 * 60 * 24);
+// let currentDate = new Date(new Date().getTime() - (36 * 60 * 60 * 1000));
 
-const getData = async () => {
-    const DATA_URL = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv';
-    await d3.csv(DATA_URL, res => {
-        let difference = Math.floor(dayDifference(currentDate, new Date(res.date))); 
-        if (difference < 14) records.push(Object.values(res));
-    }); 
-}
+// const getData = async () => {
+//     const DATA_URL = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv';
+//     await d3.csv(DATA_URL, res => {
+//         let difference = Math.floor(dayDifference(currentDate, new Date(res.date))); 
+//         if (difference < 14) records.push(Object.values(res));
+//     }); 
+// }
 
-const updateCsv = async() => {
-    await getData();
-    const csvWriter = createCsvWriter({
-        path: path.resolve(__dirname, '../../assets/covidData.csv'),
-        header: ['date', 'county', 'state', 'fips', 'cases', 'deaths']
-    });
-    await csvWriter.writeRecords(records);
-}
+// const updateCsv = async() => {
+//     await getData();
+//     const csvWriter = createCsvWriter({
+//         path: path.resolve(__dirname, '../../assets/covidData.csv'),
+//         header: ['date', 'county', 'state', 'fips', 'cases', 'deaths']
+//     });
+//     await csvWriter.writeRecords(records);
+// }
 
 export async function handler(e, context) {
     if (e.httpMethod != 'POST') return {statusCode: 404}
